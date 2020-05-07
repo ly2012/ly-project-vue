4f345703a74b4fa167f46329ea7618a8f2876edd
ly2012 <ljjly2012@163.com>
2020-05-07 15:23:45 +0800

    first

---
+++ src/views/common/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mod-home">
+    <h3>made in ly</h3>
+    <ul>
+      <li>海内存知己，天涯若比邻</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+  .mod-home {
+    line-height: 1.5;
+  }
+</style>
+
